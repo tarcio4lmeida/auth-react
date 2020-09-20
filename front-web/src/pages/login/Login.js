7b9f7d2eb37eac5550df3c0a463e2e067d2dccf0
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-09-19 21:52:12 -0300

    Implementando pagina de login

--- front-web/src/pages/login/Login.js
+++ front-web/src/pages/login/Login.js
@@ -1,6 +1,55 @@
-
 import React from 'react'
 
-const Login = () => <h1>Login</h1>
+import { ErrorMessage, Formik, Form, Field } from 'formik'
+import * as yup from 'yup'
+import axios from 'axios'
+import './Login.css'
+
+
+const Login = () => {
+    const handleSubmit = values => console.log(values)
+    const validations = yup.object().shape({
+        email: yup.string().email().required(),
+        password: yup.string().min(8).required()
+    })
+    return (
+        <>
+            <h1>Login</h1>
+            <p>Fill the fields to continue</p>
+            <Formik
+                initialValues={{}}
+                onSubmit={handleSubmit}
+                validationSchema={validations}
+            >
+                <Form className="Login">
+                    <div className="Login-Group">
+                        <Field
+                            name="email"
+                            className="Login-Field"
+                        />
+                        <ErrorMessage
+                            component="span"
+                            name="email"
+                            className="Login-Error"
+                        />
+                    </div>
+                    <div className="Login-Group">
+                        <Field
+                            name="password"
+                            className="Login-Field"
+                        />
+                        <ErrorMessage
+                            component="span"
+                            name="password"
+                            className="Login-Error"
+                        />
+                    </div>
+                    <button className="Login-Btn" type="submit">Login</button>
+                </Form>
+            </Formik>
+        </>
+    )
+}
 
 export default Login
+
